b7646433d7e72f1ad7427c72bbd6c6350f62bd76
Roman Trekhlebov <xaero_olj@mail.ru>
2020-11-26 13:41:10 +0300

    Extend model to support 2 different View protocol

--- CountryTest/MainModule/Model/Country.swift
+++ CountryTest/MainModule/Model/Country.swift
@@ -7,16 +7,50 @@
 
 import Foundation
 
-protocol CountryCellProtocol {
+typealias CountryViewProtocol = CountryMainViewProtocol & CountryDetailViewProtocol
+
+protocol CountryMainViewProtocol {
     var countryName: String {get}
     var countryCode: String {get}
 }
 
-struct Country: Codable, CountryCellProtocol {
+protocol CountryDetailViewProtocol {
+    var countryFlag: String {get} //flag (.svg)
+    var countryNativeName: String {get} //nativeName
+    var countryLanguages: [Language] {get} // languages []
+    var countryCurrencies: [Currency] {get} //currencies
+    var countryBorders: [String] {get} //borders [string]
+}
+
+struct Country: Codable, CountryViewProtocol {
     let countryName: String
     let countryCode: String
     
+    let countryFlag: String
+    let countryNativeName: String
+    let countryLanguages: [Language]
+    let countryCurrencies: [Currency]
+    let countryBorders: [String]
+    
     private enum CodingKeys : String, CodingKey {
-        case countryName = "name", countryCode = "alpha3Code"
+        case countryName = "name",
+             countryCode = "alpha3Code",
+             countryFlag = "flag",
+             countryNativeName = "nativeName",
+             countryLanguages = "languages",
+             countryCurrencies = "currencies",
+             countryBorders = "borders"
     }
 }
+
+struct Currency: Codable {
+    let code: String
+    let name: String
+    let symbol: String
+}
+
+struct Language: Codable {
+    let name: String
+    let nativeName: String
+}
+
