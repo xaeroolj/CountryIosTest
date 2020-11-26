157ad63d967522f76282bd3f8afbe00af5b4becd
Roman Trekhlebov <xaero_olj@mail.ru>
2020-11-26 22:54:59 +0300

    Router Tests

---
+++ CountryTestTests/RouterTests.swift
@@ -0,0 +1,71 @@
+//
+//  RouterTests.swift
+//  CountryTestTests
+//
+//  Created by Roman Trekhlebov on 26.11.2020.
+//
+
+import XCTest
+@testable import CountryTest
+
+class MockNavigationControll: UINavigationController {
+    var presentedVC: UIViewController!
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+
+        self.presentedVC = viewController
+
+        super.pushViewController(presentedVC, animated: animated)
+    }
+}
+
+class RouterTests: XCTestCase {
+    var router: RouterProtocol!
+    var navigatioController:MockNavigationControll!
+    var assembly: AssemblyModuleBuilder!
+
+    override func setUpWithError() throws {
+        
+        navigatioController = MockNavigationControll()
+        assembly = AssemblyModuleBuilder()
+        router = Router(navigationController: navigatioController, assemblyBuilder: assembly)
+    }
+
+    override func tearDownWithError() throws {
+        router = nil
+        navigatioController = nil
+        assembly = nil
+    }
+
+    func testInitialViewController() throws {
+        router.initialViewController()
+        let rootVC = navigatioController.viewControllers.first
+        XCTAssertTrue(rootVC is MainViewController)
+    }
+    
+    func testShowDetail() {
+        
+        let country = Country(countryName: "foo",
+                              countryCode: "baz",
+                              countryFlag: "bar",
+                              countryNativeName: "фоо",
+                              countryLanguages: [],
+                              countryCurrencies: [],
+                              countryBorders: [])
+        router.showDetail(country: country)
+        let detailVC = navigatioController.presentedVC
+        XCTAssertNotNil(detailVC)
+        XCTAssertTrue(detailVC is DetailViewController)
+    }
+
+    func testShowNewDetail() {
+        
+        router.showNewCountryDetail(code: "Foo")
+        let detailVC = navigatioController.presentedVC
+        XCTAssertNotNil(detailVC)
+        XCTAssertTrue(detailVC is DetailViewController)
+    }
+    
+    
+
+}
