df8e9381896004cc902583e3342aa38cf1001e7a
Roman Trekhlebov <xaero_olj@mail.ru>
2020-11-26 17:41:18 +0300

    simple MainPresener tests

--- CountryTestTests/MainModuleTests/MainPresenterTests.swift
+++ CountryTestTests/MainModuleTests/MainPresenterTests.swift
@@ -22,8 +22,22 @@ class MockView: MainViewProtocol {
 
 class MockDataService: CountryServiceForMainViewProtocol {
     
+    var countryes: [Country]!
+    
+    init() { }
+    convenience init(countryes: [Country]?) {
+        self.init()
+        self.countryes = countryes
+    }
+    
+    
+    
     func getCountryBy(name: String, completion: @escaping (Result<[Country], NetworkError>) -> Void) {
-        
+        if let countryes = countryes {
+            completion(.success(countryes ))
+        } else {
+            completion(.failure(.domainError))
+        }
     }
     
 }
@@ -33,40 +47,75 @@ class MainPresenterTests: XCTestCase {
     var view: MockView!
     var presenter: MainPresenter!
     var dataService: CountryServiceForMainViewProtocol!
+    var router: RouterProtocol!
+//    var countryes = [Country]()
     
     override func setUpWithError() throws {
-        view = MockView()
-        dataService = MockDataService()
-        presenter = MainPresenter(view: view, dataService: dataService)
+        let navC = UINavigationController()
+        let assamblyB = AssemblyModuleBuilder()
+        router = Router(navigationController: navC, assemblyBuilder: assamblyB)
     }
     
     override func tearDownWithError() throws {
         view = nil
         dataService = nil
         presenter = nil
+        router = nil
+    }
+    func testGetCountryBySuccess() throws {
+        let country = Country(countryName: "foo",
+                              countryCode: "baz",
+                              countryFlag: "bar",
+                              countryNativeName: "фоо",
+                              countryLanguages: [],
+                              countryCurrencies: [],
+                              countryBorders: [])
+        
+        view = MockView()
+        dataService = MockDataService(countryes: [country])
+        presenter = MainPresenter(view: view, dataService: dataService, router: router)
+        var catchCountryes: [Country]?
+        
+        dataService.getCountryBy(name: "foo") { result in
+            switch result {
+            case .success(let countryes ):
+                catchCountryes = countryes
+            case .failure(let error):
+                print(error)
+            }
+        }
+        XCTAssertNotEqual(catchCountryes?.count, 0)
+        XCTAssertEqual(catchCountryes?.first?.countryCode, "baz")
+        
     }
     
-    func testModuleIsNotNil() throws {
-        XCTAssertNotNil(view, "View is not nil")
-        XCTAssertNotNil(dataService, "dataService is not nil")
-        XCTAssertNotNil(presenter, "Presenter is not nil")
-    }
-    
-    func testView() throws {
-        presenter.dataService.getCountryBy(name: "ru") { [weak self] result in
-            guard let self = self else { return }
+    func testGetCountryByFalure() throws {
+//        let country = Country(countryName: "foo",
+//                              countryCode: "baz",
+//                              countryFlag: "bar",
+//                              countryNativeName: "фоо",
+//                              countryLanguages: [],
+//                              countryCurrencies: [],
+//                              countryBorders: [])
+        
+        view = MockView()
+        dataService = MockDataService()
+        presenter = MainPresenter(view: view, dataService: dataService, router: router)
+        
+        var catchError: NetworkError?
+        
+        dataService.getCountryBy(name: "foo") { result in
+            switch result {
+            case .success(_):
+                break
+            case .failure(let error):
+                catchError = error
+            }
         }
         
-//        XCTAssertEqual(view.model?.countryName, "Baz")
+        XCTAssertNotNil(catchError)
         
     }
-    
-//    func testModel() throws {
-//
-//        XCTAssertEqual(model.countryName, "Baz")
-//        XCTAssertEqual(model.countryCode, "Bar")
-//
-//    }
     
 
 }
