4e482ecae3ae15da97fc0a9e5e8b08d6949f2814
Roman Trekhlebov <xaero_olj@mail.ru>
2020-11-27 21:07:05 +0300

    UIApplication.shared.isNetworkActivityIndicatorVisible for NetworkService

--- CountryTest/NetworkLayer/NetworkService.swift
+++ CountryTest/NetworkLayer/NetworkService.swift
@@ -5,7 +5,7 @@
 //  Created by Roman Trekhlebov on 25.11.2020.
 //
 
-import Foundation
+import UIKit
 
 protocol Networking {
     func request(url: URL, method: HttpMethod, completion: @escaping (Data?, Error?)-> Void)
@@ -28,15 +28,24 @@ class NetworkService: Networking {
         let task = createDataTask(from: request, completion: completion)
         self.task = task
         
+        self.isLoading(true)
         self.task?.resume()
         
         
+    }
+    private func isLoading(_ status: Bool) {
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = status
+
+        }
+
     }
     
     private func createDataTask(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
         return URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
             defer {
                 self?.task = nil
+                self?.isLoading(false)
             }
             
             let response = response as! HTTPURLResponse
