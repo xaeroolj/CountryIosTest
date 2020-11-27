5a00be6a7cdab84aac3c36127a23744f4a3f1df3
Roman Trekhlebov <xaero_olj@mail.ru>
2020-11-27 20:44:29 +0300

    String Extension + UrlConstructor fix

---
+++ CountryTest/Extensions/String+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  String+Extension.swift
+//  CountryTest
+//
+//  Created by Roman Trekhlebov on 27.11.2020.
+//
+
+import UIKit
+
+
+extension String {
+    func removeWhiteSpaces() -> String {
+        return self.filter { !$0.isWhitespace }
+    }
+}
--- CountryTest/Helpers/UrlConstructor.swift
+++ CountryTest/Helpers/UrlConstructor.swift
@@ -10,10 +10,12 @@ import Foundation
 final class UrlConstructor {
 
     static func getCountryByNameUrl(name: String) -> URL? {
-        return URL(string: K.UrlStrings.countryByNameUrl.appending("\(name)"))
+        return URL(string: K.UrlStrings.countryByNameUrl.appending("\(name.removeWhiteSpaces())"))
     }
     
     static func getCountryByCodeUrl(code: String) -> URL? {
-        return URL(string: K.UrlStrings.countryByCodeUrl.appending("\(code)"))
+        return URL(string: K.UrlStrings.countryByCodeUrl.appending("\(code.removeWhiteSpaces())"))
     }
+    
+    
 }
